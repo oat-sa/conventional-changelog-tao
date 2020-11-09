59eb1aa3540aa699740b32db7e75303e8004e92c
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-11-09 18:04:54 +0100

    doc: document preset internals

--- index.js
+++ index.js
@@ -1,30 +1,56 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 Open Assessment Technologies SA;
+ */
 const parser = require('./parser.js');
 const bumper = require('./bumper.js');
 const writer = require('conventional-changelog-conventionalcommits/writer-opts.js');
 
+/**
+ * Loads and aggregate the preset options
+ * @param {Object?} presetConfig - default configuration
+ * @returns {Promise<Object>} resolves with the preset configuration
+ */
 function loadPresetOptions(presetConfig = {}) {
-    return Promise.all([parser(presetConfig), writer(presetConfig), bumper(presetConfig)]).then(
-        ([parserOpts, writerOpts, whatBump]) => ({
+    return Promise.all([
+        parser(presetConfig),
+        writer(presetConfig),
+        bumper(presetConfig)
+    ]).then(
+        ([parserOpts, writerOpts, bumpOpts]) => ({
             conventionalChangelog: { parserOpts, writerOpts },
             gitRawCommitsOpts: { noMerges: null },
             parserOpts,
             writerOpts,
-            recommendedBumpOpts: { parserOpts, whatBump }
+            recommendedBumpOpts: { parserOpts, whatBump: bumpOpts.whatBump }
         })
     );
 }
 
-module.exports = function entryPoint(presetParameter) {
-    // parameter passed can be either a config object or a callback function
+/**
+ * Get the preset configuration.
+ * Module signature is expected as a conventional changelog preset
+ * @param {Function|Object} presetParameter - either a callback or a default config
+ * @returns {Promise<Object>} resolves with the configuration
+ */
+module.exports = function getPresetConfiguration(presetParameter) {
     if (typeof presetParameter === 'function') {
-        // parameter is a callback objece
-        //
         return loadPresetOptions({})
-            .then(presetData => {
-                presetParameter(null, presetData);
-            })
+            .then(presetData => presetParameter(null, presetData))
             .catch(err => presetParameter(err));
     }
-    //parameters is an object with user defined config
     return loadPresetOptions(presetParameter || {});
 };
